fdc90530e4c67db6aba7601a6b20957cf0f54176
Henry Osorio Gómez <mo@Henrys-Air.attlocal.net>
2025-07-19 17:15:53 -0700

    Add TodoForm component and basic styling

---
+++ src/TodoList.jsx
@@ -0,0 +1,22 @@
+
+function TodoList() {
+    const todos = [
+        { id: 1, title: "review resources" },
+        { id: 2, title: "take notes" },
+        { id: 3, title: "code out app" },
+    ]
+    {/*code continues...*/ }
+
+    {/*extract from App.jsx*/ }
+    {/*...code*/ }
+    return (
+        <div>
+            <h1>Todo List</h1>
+            <ul>
+                {todos.map(todo => <li key={todo.id}>{todo.title}</li>)}
+            </ul>
+        </div>
+    );
+
+}
+export default TodoList;
