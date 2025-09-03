81b27edfbe3592f95929f7f93769ab6dcfb8cbc7
Henry Osorio Gómez <mo@Mac.attlocal.net>
2025-09-03 10:34:19 -0700

    Data Fetching Assignment

---
+++ src/features/TodoList/TextInputWithLabel.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const TextInputWithLabel = React.forwardRef(
+    ({ elementId, labelText, value, onChange }, ref) => {
+        return (
+            <div className="form-control">
+                <label htmlFor={elementId}>{labelText}</label>
+                <input
+                    id={elementId}
+                    ref={ref}
+                    type="text"
+                    value={value}
+                    onChange={onChange}
+                />
+            </div>
+        );
+    }
+);
+
+export default TextInputWithLabel;
